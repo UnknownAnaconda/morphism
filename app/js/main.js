2c50f276299648bba8f482d770f05bdd4e87b71d
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2023-11-18 18:48:00 +0300

    final

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,29 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 5,
     spaceBetween: 25,
-    loop: true,
     navigation: {
         prevEl: '.reviews__btn--prev',
         nextEl: '.reviews__btn--next'
     },
+    breakpoints: {
+        1100: {
+            slidesPerView: 5
+        },
+        870: {
+            slidesPerView: 4,
+        },
+        690: {
+            slidesPerView: 3,
+            spaceBetween: 15
+        },
+        540: {
+            slidesPerView: 2,
+            spaceBetween: 15
+        },
+        320: {
+            slidesPerView: 1
+        }
+    }
 });
+
+Fancybox.bind('[data-fancybox="video-gallery"]', {});
